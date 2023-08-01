1674fab5857594acb4c8b0580bfaf79a4591e9b0
jm-spb <tuchel.ivan@gmail.com>
2023-08-01 16:56:23 +0300

    Config decomposition - create config folder - add build folder - add config types

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,31 +1,22 @@
 import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths } from './config/build/types/config';
 
-const config:webpack.Configuration = {
-  mode: 'production',
+const paths: BuildPaths = {
   entry: path.resolve(__dirname, 'src', 'index.ts'),
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-  ],
+  build: path.resolve(__dirname, 'build'),
+  html: path.resolve(__dirname, 'public', 'index.html'),
 };
 
-export default config
+const mode = 'development';
+// instead of explicit check: if (mode === 'devepolment')
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+  mode: 'development',
+  paths,
+  isDev,
+});
+
+export default config;
