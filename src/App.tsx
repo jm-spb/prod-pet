ad6f099d7c28501682f7d57dd94a643e5f480bbc
jm-spb <tuchel.ivan@gmail.com>
2023-08-04 22:40:59 +0300

    Add react-router - create pages folder: MainPage, AboutPage - add historyApiFallback to prevent error404 on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import Counter from './components/Counter';
+import { Link, Route, Routes } from 'react-router-dom';
 import './index.scss';
+import MainPage from './pages/MainPage/MainPage';
+import AboutPage from './pages/AboutPage/AboutPage';
 
 function App() {
   return (
     <div className="app">
-      <h1>hey</h1>
-      <Counter />
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Routes>
+        <Route
+          element={<MainPage />}
+          path="/"
+        />
+        <Route
+          element={<AboutPage />}
+          path="/about"
+        />
+      </Routes>
     </div>
   );
 }
