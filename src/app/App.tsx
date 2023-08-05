af6a02cdcf6ffd22163c236c4e1f0bf97e226a4c
jm-spb <tuchel.ivan@gmail.com>
2023-08-05 23:32:44 +0300

    refactor folder structure to FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
-import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
+import { MainPageLazy } from '../pages/MainPage/MainPage.lazy';
+import { AboutPageLazy } from '../pages/AboutPage/AboutPage.lazy';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider/lib/useTheme';
+import { classNames } from '../helpers/classNames/classNames';
 
 function App() {
   const { theme, toggleTheme } = useTheme();
