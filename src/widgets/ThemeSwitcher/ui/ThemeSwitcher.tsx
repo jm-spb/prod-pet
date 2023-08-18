203bed50e41c526d2f750e98bbb0295c1cbf058a
jm-spb <tuchel.ivan@gmail.com>
2023-08-18 20:52:09 +0300

    create ThemeSwitcher widget

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import styles from './ThemeSwitcher.module.scss';
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
+import IconDark from 'shared/assets/icons/theme-dark.svg';
+import IconLight from 'shared/assets/icons/theme-light.svg';
+import { Button, VariantButton } from 'shared/ui/Button/Button';
+
+interface ThemeSwitcherProps {
+  className?: string;
+}
+
+export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = (props) => {
+  const { className } = props;
+
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      variant={VariantButton.CLEAR}
+      className={classNames(styles.ThemeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
+      {theme === Theme.DARK ? <IconDark /> : <IconLight />}
+    </Button>
+  );
+};
