26251b161ec8294a7176e5a1ddb6d61a5d52b309
jm-spb <tuchel.ivan@gmail.com>
2023-08-25 17:08:53 +0300

    create wrapper div

--- src/app/providers/router/components/AppRouter.tsx
+++ src/app/providers/router/components/AppRouter.tsx
@@ -1,11 +1,15 @@
 import { Suspense } from 'react';
-import { Route, Routes, useRoutes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => {
   const elements = useRoutes(Object.values(routeConfig));
 
-  return <Suspense fallback={<div>Loading...</div>}>{elements}</Suspense>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="page-wrapper">{elements}</div>
+    </Suspense>
+  );
 };
 
 export default AppRouter;
