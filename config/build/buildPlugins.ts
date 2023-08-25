528c80c64b4f6241960426617b0b5466494c21cd
jm-spb <tuchel.ivan@gmail.com>
2023-08-25 17:07:26 +0300

    enable HMR

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,8 +2,9 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
-export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({ template: paths.html }),
@@ -11,5 +12,11 @@ export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInst
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
+    // replaces variables with other values or expressions at compile time
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+    // allows all kinds of modules to be updated at runtime without the need for a full refresh.
+    ...(isDev ? [new ReactRefreshWebpackPlugin()] : []),
   ];
 }
