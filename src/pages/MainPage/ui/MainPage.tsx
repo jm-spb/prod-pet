19f80de108cfba0688c7afeebda19a6539be19fe
jm-spb <tuchel.ivan@gmail.com>
2023-09-18 17:36:51 +0300

    add BugButton to main page for testing purpose

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,15 @@
+import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
   const { t } = useTranslation('translation');
 
-  return <div>{t('main')}</div>;
+  return (
+    <div>
+      {t('main')}
+      <BugButton />
+    </div>
+  );
 };
 
 export default MainPage;
