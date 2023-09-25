7322d965668504826b16496dc98e04e25e0fad37
jm-spb <tuchel.ivan@gmail.com>
2023-09-25 21:19:55 +0300

    test(Button): add test clear variant

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, VariantButton } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
-  test('', () => {
+  test('test render', () => {
     render(<Button>TEST</Button>);
     expect(screen.getByText('TEST')).toBeInTheDocument();
+  });
+  test('test clear variant', () => {
+    render(<Button variant={VariantButton.CLEAR}>TEST</Button>);
+    expect(screen.getByText('TEST')).toHaveClass('clear');
+    screen.debug();
   });
 });
