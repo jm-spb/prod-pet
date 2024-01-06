530e315093bf8f7b10095e6182ac2ff84dc35305
jm-spb <tuchel.ivan@gmail.com>
2024-01-06 23:36:00 +0300

    test(Button): add test cases

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -2,13 +2,32 @@ import { render, screen } from '@testing-library/react';
 import { Button, VariantButton } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
-  test('test render', () => {
+  test('renders with default props', () => {
     render(<Button>TEST</Button>);
     expect(screen.getByText('TEST')).toBeInTheDocument();
   });
-  test('test clear variant', () => {
+
+  test('renders with clear variant', () => {
     render(<Button variant={VariantButton.CLEAR}>TEST</Button>);
     expect(screen.getByText('TEST')).toHaveClass('clear');
-    screen.debug();
+  });
+
+  test('renders with custom className', () => {
+    const className = 'custom-class';
+    render(<Button className={className}>TEST</Button>);
+    expect(screen.getByText('TEST')).toHaveClass(className);
+  });
+
+  test('calls onClick function when clicked', () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>TEST</Button>);
+    screen.getByText('TEST').click();
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('passes other props to the button element', () => {
+    const dataTestId = 'test-button';
+    render(<Button data-testid={dataTestId}>TEST</Button>);
+    expect(screen.getByTestId(dataTestId)).toBeInTheDocument();
   });
 });
