d90418f7aa3694e96affe9e66c26c3545101fe2e
jm-spb <tuchel.ivan@gmail.com>
2024-01-08 22:58:43 +0300

    refactor: create separate css loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import { RuleSetRule } from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
   // svgr - to use svg as React component
@@ -31,27 +31,7 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  const sassLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      // For dev mode use style-loader, because it injects CSS into the DOM using multiple and works faster
-      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      // Translates CSS into CommonJS
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            auto: /\.module\.\w+$/i,
-            // in devmode make explicit scss module name for easier debugging
-            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
-          },
-        },
-      },
-
-      // Compiles Sass to CSS
-      'sass-loader',
-    ],
-  };
+  const sassLoader = buildCssLoader(isDev);
 
   // A chain is executed in reverse order: typescriptLoader -> sassLoader -> ...
   return [imgLoader, svgLoader, sassLoader, babelLoader, typescriptLoader];
