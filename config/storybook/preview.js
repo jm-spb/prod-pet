b48ea117acbd2dc4c66ae7b009feefa4804e1bdf
jm-spb <tuchel.ivan@gmail.com>
2024-01-12 20:07:07 +0300

    config(storybook): add decorators

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,9 +1,16 @@
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+addDecorator(StyleDecorator);
+addDecorator(ThemeDecorator('light'));
