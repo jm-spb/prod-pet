1e45f89c14444f6107d1706057ce093ae5d40d95
jm-spb <tuchel.ivan@gmail.com>
2024-01-15 20:41:49 +0300

    refactor: remove any

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,11 +11,31 @@ export default ({ config }: { config: webpack.Configuration }) => {
     html: '',
     src: path.resolve(__dirname, '..', '..', 'src'),
   };
-
   config.resolve?.modules?.push(paths.src);
   config.resolve?.extensions?.push('.ts', '.tsx');
 
+  // set css loader
   config.module?.rules?.push(buildCssLoader(true));
+
+  /* set svgr
+   1) find storybook default svg loader in rules and exclude it
+   2) add svgr to storybook webpack config
+  */
+  if (config.module?.rules) {
+    // eslint-disable-next-line no-param-reassign
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule && rule !== '...' && /svg/.test(rule.test as string)) {
+        return { ...rule, exclude: /\.svg$/i };
+      }
+
+      return rule;
+    });
+  }
+
+  config.module?.rules?.push({
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  });
 
   return config;
 };
