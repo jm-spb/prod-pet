510e21ad7c64ee45dcce06851734fdd78477e9bb
jm-spb <tuchel.ivan@gmail.com>
2024-01-17 17:09:13 +0300

    refactor: create AppLinkColor enum

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -2,31 +2,22 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Link, LinkProps } from 'react-router-dom';
 import styles from './AppLink.module.scss';
 
-type Color = 'primary' | 'inverted';
+export enum AppLinkColor {
+  PRIMARY = 'primary',
+  INVERTED = 'inverted',
+}
 
 interface AppLinkProps extends LinkProps {
   className?: string;
-  color?: Color;
+  color?: AppLinkColor;
 }
 
 export const AppLink: React.FC<AppLinkProps> = (props) => {
   const { to, children, className, color = 'primary', ...otherProps } = props;
 
   return (
-    <Link
-      to={to}
-      className={classNames(styles.appLink, {}, [className, styles[color]])}
-      {...otherProps}
-    >
+    <Link to={to} className={classNames(styles.appLink, {}, [className, styles[color]])} {...otherProps}>
       {children}
     </Link>
   );
 };
-
-/*
-type Color = 'primary' | 'secondary'
-interface AppLinkProps extends LinkProps {
-className?: string
-color?: Color
-}
-*/
