90970e96d4b967c6907338d2ceeb0e90af017145
jm-spb <tuchel.ivan@gmail.com>
2024-01-29 22:29:10 +0300

    refactor: add button sizes and variants

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,21 +2,36 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ButtonHTMLAttributes } from 'react';
 import styles from './Button.module.scss';
 
-export enum VariantButton {
+export enum ButtonVariant {
   CLEAR = 'clear',
   OUTLINE = 'outline',
+  BACKGROUND = 'background',
+  BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export enum ButtonSize {
+  M = 'size_m',
+  L = 'size_l',
+  XL = 'size_xl',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
-  variant?: VariantButton;
+  variant?: ButtonVariant;
+  square?: boolean;
+  size?: ButtonSize;
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const { className, variant, children, ...otherProps } = props;
+  const { className, variant, children, square, size = ButtonSize.M, ...otherProps } = props;
+  const mods: Record<string, boolean> = {
+    [styles[variant]]: true,
+    [styles.square]: square,
+    [styles[size]]: true,
+  };
 
   return (
-    <button type="button" className={classNames(styles.button, {}, [className, styles[variant]])} {...otherProps}>
+    <button type="button" className={classNames(styles.button, mods, [className])} {...otherProps}>
       {children}
     </button>
   );
