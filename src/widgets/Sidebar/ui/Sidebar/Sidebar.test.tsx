f5549d27377a4d461062f957130bea57f5815ec6
jm-spb <tuchel.ivan@gmail.com>
2024-01-30 20:36:46 +0300

    test(Sidebar): create componentRender function to test components with links, add it to Sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,22 +1,22 @@
 import { screen, fireEvent } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
   test('renders without error', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('does not have "collapsed" class by default', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).not.toHaveClass('collapsed');
   });
 
   test('toggles "collapsed" class when button is clicked', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const sidebar = screen.getByTestId('sidebar');
-    const button = screen.getByText('TOGGLE');
+    const button = screen.getByText('<');
     fireEvent.click(button);
     expect(sidebar).toHaveClass('collapsed');
     fireEvent.click(button);
@@ -24,12 +24,12 @@ describe('Sidebar', () => {
   });
 
   test('renders ThemeSwitcher component', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('theme-switcher')).toBeInTheDocument();
   });
 
   test('renders LangSwitcher component', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('lang-switcher')).toBeInTheDocument();
   });
 });
