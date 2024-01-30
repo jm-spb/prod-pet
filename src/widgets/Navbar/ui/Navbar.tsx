8ab21e1f2b0d0066d0e7f2589a9df1246534cc22
jm-spb <tuchel.ivan@gmail.com>
2024-01-30 20:31:45 +0300

    refactor: move links from Navbar to Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,25 +1,12 @@
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkColor } from 'shared/ui/AppLink/AppLink';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar: React.FC<NavbarProps> = ({ className }) => {
-  const { t } = useTranslation('translation');
-
-  return (
-    <div className={classNames(styles.navbar, {}, [className])}>
-      <div className={styles.links}>
-        <AppLink to="/" color={AppLinkColor.INVERTED}>
-          {t('main')}
-        </AppLink>
-        <AppLink to="/about" color={AppLinkColor.INVERTED}>
-          {t('about')}
-        </AppLink>
-      </div>
-    </div>
-  );
-};
+export const Navbar: React.FC<NavbarProps> = ({ className }) => (
+  <div className={classNames(styles.navbar, {}, [className])}>
+    <div className={styles.links}>{/*  */}</div>
+  </div>
+);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,12 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
+import { Button, ButtonSize, ButtonVariant } from 'shared/ui/Button/Button';
+import { AppLink, AppLinkColor } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import HomeIcon from 'shared/assets/icons/home.svg';
+import AboutIcon from 'shared/assets/icons/about.svg';
 import styles from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,24 +16,36 @@ interface SidebarProps {
 
 export const Sidebar: React.FC<SidebarProps> = (props) => {
   const { className } = props;
+  const { t } = useTranslation('translation');
   const [collapsed, setCollapsed] = useState(false);
-  const toggleCollapsed = () => setCollapsed((prev) => !prev);
+  const onToggle = () => setCollapsed((prev) => !prev);
 
   return (
-    <div
-      data-testid="sidebar"
-      className={classNames(styles.sidebar, { [styles.collapsed]: collapsed }, [className])}
-    >
-      <button
-        type="button"
-        onClick={toggleCollapsed}
-      >
-        TOGGLE
-      </button>
+    <div data-testid="sidebar" className={classNames(styles.sidebar, { [styles.collapsed]: collapsed }, [className])}>
+      <div className={styles.links}>
+        <AppLink to={RoutePath.main} className={styles.link} color={AppLinkColor.INVERTED}>
+          <HomeIcon className={styles.linkIcon} />
+          <span className={styles.linkText}>{t('main')}</span>
+        </AppLink>
+        <AppLink to={RoutePath.about} className={styles.link} color={AppLinkColor.INVERTED}>
+          <AboutIcon className={styles.linkIcon} />
+          <span className={styles.linkText}>{t('about')}</span>
+        </AppLink>
+      </div>
       <div className={styles.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher />
+        <LangSwitcher collapsed={collapsed} />
       </div>
+      <Button
+        data-testid="sidebar-toggle"
+        onClick={onToggle}
+        className={styles.collapseBtn}
+        variant={ButtonVariant.BACKGROUND_INVERTED}
+        square
+        size={ButtonSize.L}
+      >
+        {collapsed ? '>' : '<'}
+      </Button>
     </div>
   );
 };
