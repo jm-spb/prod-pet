6b6b1e028187a64054f41f91375c519fb7a4c45f
jm-spb <tuchel.ivan@gmail.com>
2024-02-05 22:56:20 +0300

    feat(Portal): create Portal for modals, popovers, tooltips etc

---
+++ src/shared/ui/Portal/Portal.tsx
@@ -0,0 +1,12 @@
+import { createPortal } from 'react-dom';
+
+interface PortalProps {
+  children: React.ReactNode;
+  element?: HTMLElement;
+}
+
+export const Portal: React.FC<PortalProps> = (props) => {
+  const { children, element = document.body } = props;
+
+  return createPortal(children, element);
+};
