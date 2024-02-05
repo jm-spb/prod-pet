495c5fefb5fa9d94767f8b3b9757fa34059a1e94
jm-spb <tuchel.ivan@gmail.com>
2024-02-05 22:58:25 +0300

    feat(Navbar): add Modal to Navbar, add translations

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,12 +1,30 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { useCallback, useState } from 'react';
+import { Button, ButtonVariant } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar: React.FC<NavbarProps> = ({ className }) => (
-  <div className={classNames(styles.navbar, {}, [className])}>
-    <div className={styles.links}>{/*  */}</div>
-  </div>
-);
+export const Navbar: React.FC<NavbarProps> = ({ className }) => {
+  const [isAuthModal, setIsAuthModal] = useState(false);
+  const { t } = useTranslation('translation');
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
+  return (
+    <div className={classNames(styles.navbar, {}, [className])}>
+      <Button className={styles.links} variant={ButtonVariant.CLEAR} onClick={onToggleModal}>
+        {t('login')}
+      </Button>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit
+      </Modal>
+    </div>
+  );
+};
