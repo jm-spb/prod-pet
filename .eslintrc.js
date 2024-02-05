eac069b74472d1576f9638ea99ba867fedbc8e01
jm-spb <tuchel.ivan@gmail.com>
2024-02-05 23:03:19 +0300

    chore: install eslint-plugin-react-hooks, add rules in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'i18next'],
+  plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks'],
   rules: {
     'i18next/no-literal-string': ['error', { markupOnly: true }],
     'import/no-unresolved': 'off',
@@ -32,6 +32,11 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'no-underscore-dangle': 'off',
     'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
   },
   globals: {
     __IS_DEV__: true,
