3bbccfbf17760b3ddf38875f503b9aef91e2642f
jm-spb <tuchel.ivan@gmail.com>
2024-02-06 13:28:19 +0300

    refactor: add initial theme in theme provider & decorator

--- src/shared/config/storybook/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator.tsx
@@ -1,9 +1,14 @@
 import { Story } from '@storybook/react';
-import { Theme } from 'app/providers/ThemeProvider';
+import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) =>
-  (
-    <div className={`app ${theme}`}>
-      <StoryComponent />
-    </div>
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
+  document.documentElement.dataset.theme = theme;
+
+  return (
+    <ThemeProvider initialTheme={theme}>
+      <div className="app">
+        <StoryComponent />
+      </div>
+    </ThemeProvider>
   );
+};
