13dfa777567784c42cf92ed5211d5bfc1f320355
jm-spb <tuchel.ivan@gmail.com>
2024-02-08 21:21:59 +0300

    refactor: remove useEffect with theme change

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,19 @@
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { AppRouter } from './providers/router';
 
-const App = () => {
-  const { theme } = useTheme();
-
-  useEffect(() => {
-    document.documentElement.dataset.theme = theme;
-  }, [theme]);
-
-  return (
-    <div className={classNames('app', {})}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
-        </div>
-      </Suspense>
-    </div>
-  );
-};
+const App = () => (
+  <div className={classNames('app', {})}>
+    <Suspense fallback="">
+      <Navbar />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
+    </Suspense>
+  </div>
+);
 
 export default App;
