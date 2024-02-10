63bc066ffc630dbd1e4b8de3edede500ce7e8913
jm-spb <tuchel.ivan@gmail.com>
2024-02-10 23:45:02 +0300

    feat: add UserSchema to store

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,11 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { ReducersMapObject, configureStore } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
+import { userReducer } from 'entities/User';
 import { StateSchema } from './StateSchema';
 
 // createReduxStore - for reconfigure store in tests and storybook
 export function createReduxStore(initialState?: StateSchema) {
+  const rootReducers: ReducersMapObject<StateSchema> = {
+    counter: counterReducer,
+    user: userReducer,
+  };
+
   return configureStore<StateSchema>({
-    reducer: { counter: counterReducer },
+    reducer: rootReducers,
     devTools: __IS_DEV__,
 
     // for tests
