af9356acd988d6f05f99d1d8342453da84e247cc
jm-spb <tuchel.ivan@gmail.com>
2024-02-11 22:39:31 +0300

    refactor: remove unnecessery divs, styles. Add ability to change size, base on size prop

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -3,18 +3,42 @@ import styles from './Loader.module.scss';
 
 interface LoaderProps {
   className?: string;
+  size?: string;
 }
 
 export const Loader: React.FC<LoaderProps> = (props) => {
-  const { className } = props;
+  const { className, size = '100' } = props;
+
+  // Calculate new sizes based on the original sizes and the new size ratio
+  const newSize = parseInt(size, 10);
+  const scaleFactor = newSize / 100; // Assuming the original size is 100px
+  const scaledSize = `${newSize}px`;
+
+  const loaderStyle = {
+    width: scaledSize,
+    height: scaledSize,
+  };
+
+  const wrapperStyle = {
+    width: scaledSize,
+    height: scaledSize,
+  };
+
+  const spinnerSize = `${72 * scaleFactor}px`;
+
+  const spinnerStyle = {
+    width: spinnerSize,
+    height: spinnerSize,
+    top: `${14 * scaleFactor}px`,
+    left: `${14 * scaleFactor}px`,
+    borderWidth: `${8 * scaleFactor}px`,
+    borderColor: `var(--primary-color) transparent`,
+  };
 
   return (
-    <div className={classNames(styles.loader, {}, [className])}>
-      <div className={styles.loaderWrapper}>
-        <div />
-        <div>
-          <div />
-        </div>
+    <div className={classNames(styles.loader, {}, [className])} style={loaderStyle}>
+      <div className={styles.loaderWrapper} style={wrapperStyle}>
+        <div style={spinnerStyle} />
       </div>
     </div>
   );
