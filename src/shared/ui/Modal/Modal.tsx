85b34b5c134f14cc6faf53fe751f81021729c7eb
jm-spb <tuchel.ivan@gmail.com>
2024-02-15 11:16:43 +0300

    refactor: add lazy flag, add animation on mount/unmount

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,6 +1,5 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-// import { useTheme } from 'app/providers/ThemeProvider';
 import { Portal } from '../Portal/Portal';
 import styles from './Modal.module.scss';
 
@@ -9,14 +8,31 @@ interface ModalProps {
   children?: React.ReactNode;
   isOpen?: boolean;
   onClose?: () => void;
+  lazy?: boolean;
 }
 
 export const Modal: React.FC<ModalProps> = (props) => {
-  const { className, children, isOpen, onClose } = props;
-  //   const { theme } = useTheme();
+  const { className, children, isOpen, onClose, lazy } = props;
+
+  // Mount modal in DOM only when it has been opened
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    if (isOpen) {
+      setIsMounted(true);
+    }
+  }, [isOpen]);
+
+  // Remove modal from DOM on close after animation
+  const onAnimationEnd = () => {
+    if (!isOpen) {
+      setIsMounted(false);
+    }
+  };
+
+  // set animation when modal is opened or closed
   const mods: Record<string, boolean> = {
     [styles.opened]: isOpen,
-    // [styles[theme]]: true,
+    [styles.closed]: !isOpen,
   };
 
   const closeHandler = useCallback(() => {
@@ -25,10 +41,11 @@ export const Modal: React.FC<ModalProps> = (props) => {
     }
   }, [onClose]);
 
-  const onContentClick = (e: React.MouseEvent) => {
+  const handleContentClick = (e: React.MouseEvent) => {
     e.stopPropagation();
   };
 
+  // Close modal on escape
   useEffect(() => {
     const closeOnEscape = (e: KeyboardEvent) => {
       if (e.key === 'Escape') {
@@ -45,12 +62,16 @@ export const Modal: React.FC<ModalProps> = (props) => {
     };
   }, [isOpen, closeHandler]);
 
+  if (lazy && !isMounted) {
+    return null;
+  }
+
   return (
     <Portal element={document.getElementById('root') ?? document.body}>
-      <div className={classNames(styles.modal, mods, [className])}>
+      <div className={classNames(styles.modal, mods, [className])} onAnimationEnd={onAnimationEnd}>
         <div className={styles.overlay} onClick={closeHandler}>
-          <div className={styles.content} onClick={onContentClick}>
-            {isOpen ? children : null}
+          <div className={styles.content} onClick={handleContentClick}>
+            {children}
           </div>
         </div>
       </div>
