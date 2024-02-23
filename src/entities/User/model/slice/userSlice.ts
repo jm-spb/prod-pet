b447e1bdd8f7e47f25ee3e031f6c0a552a7a5817
jm-spb <tuchel.ivan@gmail.com>
2024-02-23 21:37:02 +0300

    feat: add setAuthData & initAuthData reducers

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,12 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { UserSchema } from '../types/user';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { LOCAL_STORAGE_USER_KEY } from 'shared/const/localStorage';
+import { User, UserSchema } from '../types/user';
 
 const initialState: UserSchema = {};
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    setAuthData: (state, action: PayloadAction<User>) => {
+      state.authData = action.payload;
+    },
+    // check if user is logged in
+    initAuthData: (state) => {
+      const user = localStorage.getItem(LOCAL_STORAGE_USER_KEY);
+      if (user) {
+        state.authData = JSON.parse(user);
+      }
+    },
+  },
 });
 
 export const { actions: userActions, reducer: userReducer } = userSlice;
