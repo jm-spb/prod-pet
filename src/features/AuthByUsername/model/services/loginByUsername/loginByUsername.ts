776aa9f3deac98229b8de6abd3464a7a28818bb4
jm-spb <tuchel.ivan@gmail.com>
2024-02-23 21:33:04 +0300

    refactor: add dispatch function to store authData. Set authData in localStorage

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { User } from 'entities/User';
+import { User, userActions } from 'entities/User';
+import i18n from 'shared/config/i18n/i18n';
+import { LOCAL_STORAGE_USER_KEY } from 'shared/const/localStorage';
 
 interface LoginByUsernameProps {
   username: string;
@@ -12,14 +14,16 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
   async (authData, thunkAPI) => {
     try {
       const response = await axios.post<User>('http://localhost:8000/login', authData);
-
       if (!response.data) {
         throw new Error();
       }
 
+      // TODO: refactor this auth imitation to real auth with tokens
+      localStorage.setItem(LOCAL_STORAGE_USER_KEY, JSON.stringify(response.data));
+      thunkAPI.dispatch(userActions.setAuthData(response.data));
       return response.data;
     } catch (e) {
-      return thunkAPI.rejectWithValue(e);
+      return thunkAPI.rejectWithValue(i18n.t('login_error'));
     }
   }
 );
