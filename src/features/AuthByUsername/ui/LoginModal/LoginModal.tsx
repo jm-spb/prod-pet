a6d116101bedd23e91b805446513059ce814b8e6
jm-spb <tuchel.ivan@gmail.com>
2024-02-25 09:43:03 +0300

    refactor: remove wrapper, translations from LoginModal

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
@@ -12,14 +11,10 @@ interface LoginModalProps {
 
 export const LoginModal: React.FC<LoginModalProps> = (props) => {
   const { className, isOpen, onClose } = props;
-  const { t } = useTranslation('translation');
 
   return (
     <Modal className={classNames(styles.loginModal, {}, [className])} isOpen={isOpen} onClose={onClose} lazy>
-      <div className={styles.wrapper}>
-        <span className={styles.heading}>{t('auth')}</span>
-        <LoginForm />
-      </div>
+      <LoginForm />
     </Modal>
   );
 };
