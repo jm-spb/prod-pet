aae76c6d293b78f8a955c284715c211070a52774
jm-spb <tuchel.ivan@gmail.com>
2024-02-28 21:09:40 +0300

    fix: setup json-server cors

--- json-server/index.js
+++ json-server/index.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 const jsonServer = require('json-server');
-// const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const path = require('path');
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+
+server.use(jsonServer.defaults());
+server.use(jsonServer.bodyParser);
+
+server.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  })
+);
+server.options('*', cors());
 
 // imitation backend
 server.use(async (req, res, next) => {
@@ -16,30 +28,28 @@ server.use(async (req, res, next) => {
 
 // check auth
 // eslint-disable-next-line consistent-return
-server.use((req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(403).json({ message: 'AUTH ERROR' });
-  }
-  next();
-});
-
-server.use(jsonServer.defaults());
-server.use(router);
+// server.use((req, res, next) => {
+//   if (!req.headers.authorization) {
+//     return res.status(403).json({ message: 'AUTH ERROR' });
+//   }
+//   next();
+// });
 
 // login endpoint
-server.post('login', (req, res) => {
+server.post('/login', (req, res) => {
   const { username, password } = req.body;
   const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
-  const { users = [] } = db;
-
+  const { users } = db;
   const userFromBd = users.find((u) => u.username === username && u.password === password);
 
   if (userFromBd) {
-    return res.jsonp(userFromBd);
+    return res.json(userFromBd);
   }
 
-  return res.status(403).json({ message: 'AUTH ERROR' });
+  return res.status(403).json({ message: 'User not found' });
 });
+
+server.use(router);
 
 // server start
 server.listen(8000, () => {
