92dee3ae8923d6f1436c7ad4ba275c9a93a3f1d1
jm-spb <tuchel.ivan@gmail.com>
2024-06-12 15:47:42 +0300

    feat: add conditional rendering, based on user isAuth

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,11 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button, ButtonVariant } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 import { LoginModal } from 'features/AuthByUsername';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { userActions } from 'entities/User';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -12,16 +15,40 @@ interface NavbarProps {
 export const Navbar: React.FC<NavbarProps> = ({ className }) => {
   const [isAuthModal, setIsAuthModal] = useState(false);
   const { t } = useTranslation('translation');
-  const handleModalOpen = useCallback(() => {
-    setIsAuthModal((prev) => !prev);
-  }, []);
+  const dispatch = useDispatch();
+  const { authData } = useSelector((state: StateSchema) => state.user);
+
+  const handleLogout = () => {
+    dispatch(userActions.logoutUser());
+  };
+
+  const handleModalOpen = () => {
+    setIsAuthModal(true);
+  };
+
+  const handleModalClose = () => {
+    setIsAuthModal(false);
+    // TODO: create dispatch to clear form
+  };
+
+  if (authData) {
+    if (isAuthModal) setIsAuthModal(false);
+
+    return (
+      <div className={classNames(styles.navbar, {}, [className])}>
+        <Button className={styles.links} variant={ButtonVariant.CLEAR} onClick={handleLogout}>
+          {t('logout')}
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
       <Button className={styles.links} variant={ButtonVariant.CLEAR} onClick={handleModalOpen}>
         {t('login')}
       </Button>
-      <LoginModal isOpen={isAuthModal} onClose={handleModalOpen} />
+      <LoginModal isOpen={isAuthModal} onClose={handleModalClose} />
     </div>
   );
 };
