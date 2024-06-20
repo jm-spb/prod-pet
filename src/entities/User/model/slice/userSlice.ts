36278972c98b6914ca9e67f0bf9051cb48f80c4e
jm-spb <tuchel.ivan@gmail.com>
2024-06-21 00:31:55 +0300

    refactor: removed local storage from reducers

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,5 +1,4 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { LOCAL_STORAGE_USER_KEY } from 'shared/const/localStorage';
 import { User, UserSchema } from '../types/user';
 
 const initialState: UserSchema = {};
@@ -8,20 +7,13 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setAuthData: (state, action: PayloadAction<User>) => {
+    // using redux middleware to check auth data in Local Storage
+    loadUserFromLocalStorage: () => {},
+    setUserAuthData: (state, action: PayloadAction<User>) => {
       state.authData = action.payload;
     },
-    // check if user is logged in
-    initAuthData: (state) => {
-      // TODO: access to localStorage in reducer is antipattern, remove it
-      const user = localStorage.getItem(LOCAL_STORAGE_USER_KEY);
-      if (user) {
-        state.authData = JSON.parse(user);
-      }
-    },
     logoutUser: (state) => {
-      state.authData = undefined;
-      localStorage.removeItem(LOCAL_STORAGE_USER_KEY);
+      state.authData = null;
     },
   },
 });
