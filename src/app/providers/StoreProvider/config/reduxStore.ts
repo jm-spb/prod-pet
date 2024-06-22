8466e374dd77106d22296355ceb6ab4630115c25
jm-spb <tuchel.ivan@gmail.com>
2024-06-22 23:46:45 +0300

    feat(redux store): added createReducerManager to store

---
+++ src/app/providers/StoreProvider/config/reduxStore.ts
@@ -0,0 +1,30 @@
+import { ReducersMapObject, configureStore } from '@reduxjs/toolkit';
+import { counterReducer } from 'entities/Counter';
+import { userReducer } from 'entities/User';
+import { StateSchema } from './reduxStoreTypes';
+import { authMiddleware } from '../middleware/authMiddleware';
+import { createReducerManager } from './reducerManager';
+
+// createReduxStore - for reconfigure store in tests and storybook
+export function createReduxStore(initialState?: StateSchema) {
+  const rootReducers: ReducersMapObject<StateSchema> = {
+    counter: counterReducer,
+    user: userReducer,
+  };
+
+  const reducerManager = createReducerManager(rootReducers);
+
+  const reduxStore = configureStore({
+    reducer: reducerManager.reduce,
+    devTools: __IS_DEV__,
+
+    // preloadedState - for tests
+    preloadedState: initialState,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authMiddleware),
+  });
+
+  // @ts-ignore
+  reduxStore.reducerManager = reducerManager;
+
+  return reduxStore;
+}
