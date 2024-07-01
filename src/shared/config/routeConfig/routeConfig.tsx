f5c0b1d8b457b77f8dcbf971fbb35f4e11471aff
jm-spb <tuchel.ivan@gmail.com>
2024-07-01 09:54:29 +0300

    feat: added Profile page route

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,17 +1,20 @@
 import { RouteObject } from 'react-router-dom';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { ProfilePage } from 'pages/ProfilePage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  PROFILE = 'profile',
   NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.PROFILE]: '/profile',
   [AppRoutes.NOT_FOUND]: '*',
 };
 
@@ -23,6 +26,10 @@ export const routeConfig: Record<AppRoutes, RouteObject> = {
   [AppRoutes.ABOUT]: {
     path: RoutePath[AppRoutes.ABOUT],
     element: <AboutPage />,
+  },
+  [AppRoutes.PROFILE]: {
+    path: RoutePath[AppRoutes.PROFILE],
+    element: <ProfilePage />,
   },
   [AppRoutes.NOT_FOUND]: {
     path: RoutePath[AppRoutes.NOT_FOUND],
