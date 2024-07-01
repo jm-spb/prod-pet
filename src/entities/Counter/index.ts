b0ec01b7de73770d1d892b3acb2cce6b824be3ed
jm-spb <tuchel.ivan@gmail.com>
2024-07-01 10:01:22 +0300

    chore(translations): added profile page tranlations

--- src/entities/Counter/index.ts
+++
@@ -1,5 +0,0 @@
-import { counterReducer } from './model/slice/counterSlice';
-import { Counter } from './ui/Counter';
-import { CounterSchema } from './model/types/counterSchema';
-
-export { counterReducer, CounterSchema, Counter };
